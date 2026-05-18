907568a811e7633e72951effb8010c53d798d39d
Your Name <shahinmohamed371@gmail.com>
2026-05-18 23:04:40 +0300

    newww

--- main.js
+++ main.js
@@ -169,7 +169,7 @@ function playIntroSound() {
 // =========================
 // 🪄 NAME WITH WAND + SMOKE
 // =========================
-const herName = "Habiba to Gryffindor... 🦁";
+const herName = "Habiba to Gryffindor.🦁";
 
 function showNameCinematic() {
   const el = document.getElementById("nameText");
